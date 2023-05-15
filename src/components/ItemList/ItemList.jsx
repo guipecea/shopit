28b7091bfcabc05cc4622976799c014c802e1443
Guillermo Perez <guipecea@gmail.com>
2023-05-15 16:06:46 -0300

    Ajustes varios

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -8,13 +8,13 @@ const ItemList = ({products}) => {
     return(
         <div>        
             <Row xs={1} md={3} className="g-4">
-                {/* Aca hacemos uso de la funcion map, para que por cada producto se devuelva un objeto del component card */}                
+                {/* Aca hacemos uso de la funcion map, para que por cada producto se devuelva un objeto del component Item en el tag Col */}
                 {products.map((prod) => (
                     <Col key={prod.id}>
                         <Item key={prod.id} {...prod}/>
                     </Col>
                 ))}
-            </Row>            
+            </Row>
         </div>
     )
 
